341e5238fe61a276a612baacb2e140ee0fc2e9ae
solomonadzape95 <adzapesolomon@gmail.com>
2026-01-03 23:26:10 +0100

    add intro.cpp with main function and greet implementation

---
+++ cpp/intro.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//forward declaration
+string greet(int hourTime);
+
+int main() {
+  int a, b;
+  cout << "Enter 2 numbers: ";
+  cin >> a >> b;
+  cout << "The sum is: " << a + b << endl;
+  cout << greet(13);
+  return 0;
+}
+
+string greet(int hourTime) {
+if(hourTime < 12) return "Good Morning";
+else return "Good Afternoon";
+}
